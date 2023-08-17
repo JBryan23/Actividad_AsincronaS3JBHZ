1026907848bb2297f273d7ad1c15bb42ee039aca
Josue Bryan Hernandez Zelaya <124007402+JBryan23@users.noreply.github.com>
2023-08-17 12:08:12 -0600

    Add files via upload

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Ejercicio 2:
+//Utilizando los conocimientos de declaración e inicialización de arreglos, hacer lo siguiente:
+//1.Declarar un arreglo con el tamaño de su nombre completo (nombres y apellidos, como mínimo 1 nombre y 1 apellido) e inicializarlo con sus valores respectivos
+//(nombres y apellidos).
+//2.Imprimir cada uno de los valores de sus posiciones.
+//3. Almacenar la longitud o tamaño del arreglo en una variable.
+//4. Imprimir el tamaño del arreglo y sus respectivos valores
+
+String[] MiNombreCompleto = { "Josue", "Bryan", "Hernandez", "Zelaya" };
+Console.WriteLine("VALORES");
+foreach (var arreglo  in MiNombreCompleto) { Console.WriteLine(arreglo); }
+int Tamaño_Arreglo = MiNombreCompleto.Length;
+Console.WriteLine($"El tamaño de arreglo: {Tamaño_Arreglo}");
+Console.WriteLine("El tamaño de arreglo junto sus valores");
+for (int i = 0; i < Tamaño_Arreglo; i++) { Console.WriteLine($"Su posicion es {i + 1}->{MiNombreCompleto[i]}"); }
